cde1eacb20bbf34a8ffc0bf112e7990e020cb330
Hoai Sang <hoai.sang050@gmail.com>
2022-02-11 23:18:43 +0700

    add href to a

--- components/ItemResult.js
+++ components/ItemResult.js
@@ -5,7 +5,11 @@ function ItemResult({ item }) {
   return (
     <li className='mb-5 p-4 rounded cursor-pointer bg-gradient-to-r from-slate-900'>
       <p className='text-[10px] text-gray-400 lined-clamp'>{formattedUrl}</p>
-      <h1 className='font-[500] hover:underline text-blue-300 text-xl mb-2 mt-1'>
+      <h1
+        onClick={() => {
+          window.location.href = formattedUrl
+        }}
+        className='font-[500] hover:underline text-blue-300 text-xl mb-2 mt-1'>
         {title}
       </h1>
       <p className='text-gray-300 text-[14px]'>{snippet}</p>
--- components/Search.jsx
+++ components/Search.jsx
@@ -37,22 +37,40 @@ export default function Search() {
       <div className='absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%]'>
         <div className='grid grid-cols-3 gap-5 text-[14px] md:text-[16px]'>
           <p className='item-search'>
-            <span>Images</span>
+            <a href='https://www.google.com/imghp?hl=en' target='_blank'>
+              Images
+            </a>
           </p>
           <p className='item-search'>
-            <span>Maps</span>
+            <a
+              href='https://www.google.com/maps/@9.779349,105.6189045,11z?hl=vi-VN'
+              target='_blank'>
+              Maps
+            </a>
           </p>
           <p className='item-search'>
-            <span>Docs</span>
+            <a href='https://docs.google.com/document/u/0/' target='_blank'>
+              Docs
+            </a>
           </p>
           <p className='item-search'>
-            <span>Email</span>
+            <a
+              href='https://www.google.com/intl/vi/gmail/about/'
+              target='_blank'>
+              Email
+            </a>
           </p>
           <p className='item-search'>
-            <span>Drive</span>
+            <a
+              href='https://www.google.com.vn/drive/about.html'
+              target='_blank'>
+              Drive
+            </a>
           </p>
           <p className='item-search'>
-            <span>Slides</span>
+            <a href='https://www.google.com/slides/about/' target='_blank'>
+              Slides
+            </a>
           </p>
         </div>
       </div>
